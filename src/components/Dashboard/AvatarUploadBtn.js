8228ff8fd25941bbf3be883daeee46120f3a4d73
Deepak Kumar <deepak748797@gmail.com>
2022-08-01 04:06:05 +0530

    completed the avatar uploading funcationality

--- src/components/Dashboard/AvatarUploadBtn.js
+++ src/components/Dashboard/AvatarUploadBtn.js
@@ -4,6 +4,7 @@ import AvatarEditor from 'react-avatar-editor';
 import { useModalState } from '../../misc/custom.hooks';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../Context/profile.provider';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/jpg'];
@@ -69,6 +70,11 @@ export default function AvatarUploadBtn() {
 
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar
+        src={profile.avatar}
+        name={profile.name}
+        className="width-200 height-200 img-fullsize font-huge"
+      />
       <div>
         <label
           htmlFor="avatar-upload"
