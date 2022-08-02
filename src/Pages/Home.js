46142ba5aaaced2282d97958d4e85ea3c51eea23
Deepak Kumar <deepak748797@gmail.com>
2022-08-03 00:54:25 +0530

    created rooms context

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Col, Grid, Row } from 'rsuite';
+import { RoomsProvider } from '../Context/Rooms.context';
 import Sidebar from './Sidebar';
 
 export default function Home() {
   return (
-    <Grid fluid className="h-100">
-      <Row className="h-100">
-        <Col className="h-100" xs={24} md={8}>
-          <Sidebar />
-        </Col>
-      </Row>
-    </Grid>
+    <RoomsProvider>
+      <Grid fluid className="h-100">
+        <Row className="h-100">
+          <Col className="h-100" xs={24} md={8}>
+            <Sidebar />
+          </Col>
+        </Row>
+      </Grid>
+    </RoomsProvider>
   );
 }
